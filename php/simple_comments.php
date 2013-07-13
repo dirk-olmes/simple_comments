3105948717768ad333714b495dca7b39e5c02793
Dirk Olmes <dirk.olmes@mulesoft.com>
2013-07-13 13:58:12 +0200

    add the php script to submit comments

---
+++ php/simple_comments.php
@@ -0,0 +1,51 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $comment_dir = '/tmp';
+
+    $slug = htmlspecialchars($_POST['Slug']);
+    if (empty($slug))
+    {
+        die('POST request did not include value for required key "Slug"');
+    }
+    // make sure that any '/' characters inside the slug does not create directories
+    $slug = str_replace('/', '_', $slug);
+
+    $date = date('Y-m-d H:i:s');
+    $author = htmlspecialchars($_POST['Author']);
+    $comment = htmlspecialchars($_POST['Comment']);
+
+    $dir = $comment_dir . '/' . $slug;
+    if (is_dir($dir) == False)
+    {
+        mkdir($dir, 0755);
+    }
+
+    $filename = $dir . '/' . uniqid();
+
+    $handle = fopen($filename, 'w');
+    fwrite($handle, 'Author: ');
+    fwrite($handle, $author);
+    fwrite($handle, PHP_EOL);
+    fwrite($handle, 'Date: ');
+    fwrite($handle, $date);
+    fwrite($handle, PHP_EOL);
+    fwrite($handle, PHP_EOL);
+    fwrite($handle, $comment);
+    fwrite($handle, PHP_EOL);
+    fclose($handle);
+?>
+
+<html>
+    <head>
+        <title>Your comment was posted</title>
+    </head>
+    <body>
+        <h1>Thank you</h1>
+        Your comment was posted and will appear on the blog as soon as a moderator has reviewed it.
+    </body>
+</html>
+
+<?php
+}
+?>
